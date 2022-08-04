06213f5e2f7fc0007398bda8ca610510ff444885
Kyle Anson <k-anson@github.com>
2022-08-04 10:51:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,5 @@ set -x
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
-ln -s $(pwd)/emacs $HOME/.emacs
-ln -s $(pwd)/screenrc $HOME/.screenrc
 
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
