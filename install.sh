a7ca3dd56ac38dc4b98a520bb595aa8250ed73ff
Kyle Anson <k-anson@github.com>
2022-08-29 12:01:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
