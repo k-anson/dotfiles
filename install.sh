97b564192e98a289f38dff02c374cc9e6915fef0
Kyle Anson <k-anson@github.com>
2022-09-26 11:02:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
+sudo apt-get update -y
+
 brew install nvim lazygit
 
 nvim --headless +PlugInstall
@@ -11,6 +13,7 @@ nvim --headless +PlugInstall
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
+mkdir $HOME/.config/nvim
 ln -s $(pwd)/nvim/init.vim $HOME/.config/nvim/init.vim
 
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
