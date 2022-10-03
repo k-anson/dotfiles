ac05b55a57e704d07f6a52d420308446b36bc042
Kyle Anson <k-anson@Kyles-Work-Macbook.local>
2022-10-03 11:37:21 -0400

    nvim changes

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ exec 2>&1
 set -x
 
 sudo apt-get update -y
-sudo apt-get install -y stow
+sudo apt-get install -y stow fzf
 
 BREW=/home/linuxbrew/.linuxbrew/bin/brew
 $BREW install neovim
--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -1,3 +1,7 @@
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath=&runtimepath
 source ~/.vimrc
+
+lua <<EOF
+require('telecope').load_extension('fzf')
+EOF
