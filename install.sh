bc3d02caf9ce5b556d94de954211814604d6e766
Kyle Anson <k-anson@github.com>
2022-10-03 10:39:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,9 @@ exec 2>&1
 set -x
 
 sudo apt-get update -y
+sudo apt-get install -y stow
 
-brew install nvim lazygit
-
+brew install neovim lazygit
 nvim --headless +PlugInstall
 
-ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
-ln -s $(pwd)/vimrc $HOME/.vimrc
-ln -s $(pwd)/vim $HOME/.vim
-mkdir $HOME/.config/nvim
-ln -s $(pwd)/nvim/init.vim $HOME/.config/nvim/init.vim
-
-vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
+stow --target=$HOME nvim vim tmux
