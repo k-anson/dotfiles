868f174902ab583a2ccc25703a53ed1dfcaa256e
Kyle Anson <k-anson@github.com>
2023-02-10 16:56:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,7 @@ sudo apt-get install -y fzf
 
 # Install neovim
 curl -LO "https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb"
-#tar xf nvim-linux64.tar.gz
 sudo apt install ./nvim-linux64.deb
-#sudo ln -s ./nvim-linux64/bin/nvim /usr/local/bin
 
 # Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
@@ -20,10 +18,9 @@ curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/lates
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
-#stow --target=$HOME nvim tmux vim
 mkdir -p ~/.config
 ln -s $(pwd)/nvim ~/.config/
 ln -s $(pwd)/tmux/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/vim/init.vim ~/init.vim
+ln -s $(pwd)/vim/.vimrc ~/.vimrc
 
 nvim --headless +PlugInstall +qall
