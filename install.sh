43fa8ad24fae7cb7801be8cc96dbf07a1018d911
Kyle Anson <k-anson@github.com>
2023-02-10 12:26:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ set -x
 
 # Update system and install base packages
 sudo apt-get update -y
-sudo apt-get install -y stow fzf
+sudo apt-get install -y stow fzf neovim lazygit
 
 # Install neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-./squashfs-root/AppRun --version
+#curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+#chmod u+x nvim.appimage
+#./nvim.appimage --appimage-extract
+#./squashfs-root/AppRun --version
 # Install lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+#LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+#curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+#tar xf lazygit.tar.gz lazygit
+#sudo install lazygit /usr/local/bin
 
 stow --target=$HOME nvim tmux vim
 
