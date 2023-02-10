9d2d1445d45c0429ef06462adab412518aba9198
Kyle Anson <k-anson@github.com>
2023-02-10 12:20:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,20 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
+# Update system and install base packages
 sudo apt-get update -y
 sudo apt-get install -y stow fzf
 
-BREW=/home/linuxbrew/.linuxbrew/bin/brew
-$BREW install neovim
-$BREW install lazygit
+# Install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
 
 stow --target=$HOME nvim tmux vim
 
-/home/linuxbrew/.linuxbrew/bin/nvim --headless +PlugInstall +qall
+nvim --headless +PlugInstall +qall
