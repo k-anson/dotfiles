6e73eb683e9b328b2511370145f5d8f4bd55475a
Kyle Anson <k-anson@github.com>
2023-02-10 13:59:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ sudo apt-get install -y stow fzf
 # Install neovim
 curl -LO "https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz"
 tar xf nvim-linux64.tar.gz nvim
+ls
 sudo install nvim /usr/local/bin
 #sudo ln -s ./nvim-linux64/bin/nvim /usr/local/bin
 #curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
@@ -22,6 +23,7 @@ sudo install nvim /usr/local/bin
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
+ls
 sudo install lazygit /usr/local/bin
 
 stow --target=$HOME nvim tmux vim
