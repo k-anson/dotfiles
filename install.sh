d301c8b3e6958a5096681462d2b33881aea71234
Kyle Anson <k-anson@github.com>
2025-05-20 10:16:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get update -y
 sudo apt-get --assume-yes install fzf tmux
 
 # Install neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/dowload/nvim-linux-x86_64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm ./nvim-linux-x86_64.deb
